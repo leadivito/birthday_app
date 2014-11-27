bc88d333e3e86b817a10fcfb7c00ebc1ef43786e
leadivito <magikalea90@gmail.com>
2014-11-27 18:38:34 +0100

    27\11_Lea

--- SampleFacebookBirthdayApp/Models/MyAppUser.cs
+++ SampleFacebookBirthdayApp/Models/MyAppUser.cs
@@ -16,10 +16,10 @@ namespace SampleFacebookBirthdayApp.Models
         //[FacebookFieldModifier("type(large)")] // This sets the picture size to large.
         public FacebookConnection<FacebookPicture> ProfilePicture { get; set; }
 
-        //[FacebookFieldModifier("limit(8)")] // This sets the size of the friend list to 8, remove it to get all friends.
+        [FacebookFieldModifier("limit(8)")] // This sets the size of the friend list to 8, remove it to get all friends.
         public FacebookGroupConnection<MyAppUserFriend> Friends { get; set; }
 
-        //[FacebookFieldModifier("limit(16)")] // This sets the size of the photo list to 16, remove it to get all photos.
+        [FacebookFieldModifier("limit(16)")] // This sets the size of the photo list to 16, remove it to get all photos.
         public FacebookGroupConnection<FacebookPhoto> Photos { get; set; }
     }
 }
